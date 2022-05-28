c94d0facd078f986458a290be652c483d3c4633d
Eroslone <3079758191@qq.com>
2022-05-28 08:25:27 +0000

    by Eroslone

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
 <body>
 <div class="container">
     <div class="content">
-        <div class="title">Hello Laravel! - by Eros</div>
+        <div class="title">Hello Laravel! - by Eroslone</div>
     </div>
 </div>
 </body>
